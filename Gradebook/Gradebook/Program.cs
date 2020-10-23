9d9f95f9ffd8c1f623b467fbfc42feff7d108f1e
cpstelly <cpstelly@ttcu.org>
2020-10-23 07:22:31 -0500

    Add Initial Code For Gradebook

--- Gradebook/Gradebook/Program.cs
+++ Gradebook/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -6,7 +7,24 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            
+            List<double> grades = new List<double>() { 3.2, 53.2 };
+
+            double result = 0.0;
+            double highGrade = double.MinValue;
+            double lowGrade = double.MaxValue;
+
+            foreach (double number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
