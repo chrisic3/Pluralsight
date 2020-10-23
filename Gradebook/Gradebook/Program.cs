b2df6f50891307ed4289468b85bac2a3919d5c57
cpstelly <cpstelly@ttcu.org>
2020-10-23 07:25:52 -0500

    Add Book Class
    Added name and grades properties.
    Added default constructor and AddGrades method.

--- Gradebook/Gradebook/Program.cs
+++ Gradebook/Gradebook/Program.cs
@@ -7,21 +7,23 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            Book book = new Book("1st Gradebook");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
             List<double> grades = new List<double>() { 3.2, 53.2 };
 
             double result = 0.0;
             double highGrade = double.MinValue;
             double lowGrade = double.MaxValue;
-
             foreach (double number in grades)
             {
                 lowGrade = Math.Min(number, lowGrade);
                 highGrade = Math.Max(number, highGrade);
                 result += number;
             }
-
             result /= grades.Count;
-
             Console.WriteLine($"The lowest grade is {lowGrade:N1}");
             Console.WriteLine($"The highest grade is {highGrade:N1}");
             Console.WriteLine($"The average grade is {result:N1}");
